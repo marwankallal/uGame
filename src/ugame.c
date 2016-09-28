819515f3e018f7646bb654570450461172dcc0e0
Marwan Kallal <marwan.kallal@gmail.com>
2016-09-28 13:49:06 -0400

    added init and tick

--- src/ugame.c
+++ src/ugame.c
@@ -1,5 +1,18 @@
 #include "ugame.h"
+#include <inttypes.h>
 
 int main(){
+    uint8_t quit = 0;
+    
+    /* Initialization */
+
+    //TODO:Parse .ug file
+    //TODO:Open window and initialize rendering engine
+
+    /* Tick */
+    while(!quit){
+
+    }
+    
     return 0;
 }
